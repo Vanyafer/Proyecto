2640489d3e1ba81fe20795bdcc4384878da198e6
Vanyafer <33906074+Vanyafer@users.noreply.github.com>
2018-01-08 10:51:55 -0600

    por si la cago y sse borra lo demas

--- Iniciar_Sesion.php
+++ Iniciar_Sesion.php
@@ -1,1 +1,28 @@
-x
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inciar sesion</title>
+
+	<link rel="stylesheet" type="text/css" href="Diseno.css">
+</head>
+<body>
+<div id="Sesion">
+
+        <h1>Iniciar sesion</h1>
+
+        <fieldset>
+
+            <form action="javascript:void(0);" method="get">
+
+                <input type="email" value="Correo" onBlur="if(this.value=='')this.value='Correo'" onFocus="if(this.value=='Correo')this.value='' "> <!-- JS because of IE support; better: placeholder="Email" -->
+
+                <input type="password" value="Password" onBlur="if(this.value=='')this.value='Password'" onFocus="if(this.value=='Password')this.value='' "> <!-- JS because of IE support; better: placeholder="Password" -->
+
+                <input type="submit" value="Aceptar">
+            </form>
+
+        </fieldset>
+
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-hola
-holi
-holi
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nombre de nuestra pagina</title>
+	<link rel="stylesheet" type="text/css" href="Diseno.css">
+</head>
+<body>
+<div id="Barra">
+	<a href="Iniciar_Sesion.php">Iniciar Sesion</a>
+    <a href="Registro.php">Registro</a>
+</div>
+</body>
+</html>
